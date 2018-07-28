b285c6a8363d3abf1cfbe3bb2b4170952d23a0b5
maxencejded <maxence.jded@gmail.com>
2018-07-27 21:43:23 -0700

    Edit ft_strlcpy.c

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: mjacques <mjacques@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/25 23:40:47 by mjacques          #+#    #+#             */
-/*   Updated: 2018/07/26 14:37:26 by mjacques         ###   ########.fr       */
+/*   Updated: 2018/07/27 21:42:41 by mjacques         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,8 @@ char	*ft_strlcpy(const char *src, size_t len)
 
 	dst = (char *)malloc(sizeof(char) * (len + 1));
 	i = -1;
-	if (len != 1)
-	{
-		while (src[++i] && i < len)
-			dst[i] = src[i];
-		dst[i] = '\0';
-	}
-	else
-		dst[0] = '\0';
+	while (src[++i] && i < len)
+		dst[i] = src[i];
+	dst[i] = '\0';
 	return (dst);
 }
